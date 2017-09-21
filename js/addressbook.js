f6a5c5ed0c0758a29a5d7ec9d0ff7ca08e9d313e
Barry G4HDU <G4HDU@users.noreply.github.com>
2017-09-21 12:51:57 +0100

    Format js

--- js/addressbook.js
+++ js/addressbook.js
@@ -22,29 +22,19 @@ $( document ).ready(function() {
         event.stopImmediatePropagation();
         var editURL=$("#modallink").attr('href');
         var editID = $(this).attr('class').replace('addressBookRow editID', ''); // get the id of the reord
-        
-
-   $.get(myStylesLocation, function(cssTo)
-            {
-                $('<style type="text/css"></style>')
-                    .html(cssTo)
-                    .appendTo("head");
-            });
-$('#myAddrModal').modal('show');
+        $.get(myStylesLocation, function(cssTo){
+            $('<style type="text/css"></style>')
+                .html(cssTo)
+                .appendTo("head");
+        }); // end get myStylesLocation
+        $('#myAddrModal').modal('show');
         $("#modalContent").hide(0);
         $("#ajaxSpinner").show(0);
                  
         $("#modalContent").load(editURL+editID,function(){
             $("#ajaxSpinner").fadeOut(1000,function(){
                 $("#modalContent").fadeIn(1500);
-                });
-            
-       });
-
-    
-       // alert(editID);
-    });
-    
-
-        
-});
+                });  // end ajax spinner    
+       });  // end modalContent load
+    });  // end addressbookrow click
+});  // end document ready
